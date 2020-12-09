62ef2a5d6bfe8bcfb80c4555559e74869fb33cf2
Tomer Fueredi <tomerf@cs.huji.ac.il>
2020-12-09 13:05:45 +0200

    double player adjustments 1

--- Assets/Scripts/BodyScript.cs
+++ Assets/Scripts/BodyScript.cs
@@ -6,7 +6,13 @@ public class BodyScript : MonoBehaviour
 {
     public float spawnDistance;
     public GameObject linkPrefab;
-    public List<GameObject> links;
+    public JachManager manager;
+    public static LinkedList<GameObject> links;
+    public static GameObject midLink;
+    public static GameObject lastLink1;
+    public static GameObject lastLink2;
+    public int playerNum;
+    public float moveForce;
     private GameObject playerParent;
     Rigidbody2D headBody;
     
@@ -17,53 +23,75 @@ public class BodyScript : MonoBehaviour
     public KeyCode right;
     public KeyCode left;
 
+
+    private void Awake()
+    {
+        midLink = GameObject.Find("mid_link");
+        lastLink1 = GameObject.Find("JLink_Hinge1");
+        lastLink2 = GameObject.Find("JLink_Hinge2");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        links = new List<GameObject>();
         moveForce = 30f;
         headBody = GetComponent<Rigidbody2D>();
         playerParent = transform.parent.gameObject;
-        for (int i = 0; i < 5; i++)
-        {
-            addLink();
-        }
+        //for (int i = 0; i < 5; i++)
+        //{
+        //    addLink();
+        //}
     }
 
     void addLink()
     {
-        Vector2 lastPos;
-        Vector2 spawnPos;
-        GameObject last;
-        Quaternion spawnRot;
-        if (links.Count == 0)
-        {
-            last = this.gameObject;
-            if (headBody.velocity.magnitude > 0)
-            {
-                spawnPos = headBody.velocity;
-                spawnPos.Normalize();
-            }
-            else spawnPos = Vector2.left;
-            spawnPos = new Vector2(transform.position.x, transform.position.y) - (spawnPos * spawnDistance);
-            spawnRot = transform.rotation;
-        }
-        else
-        {
-            last = links[links.Count - 1];
-            lastPos = last.transform.position;
-            Vector2 beforeLastPos;
-            if (links.Count > 1) beforeLastPos = links[links.Count - 2].transform.position;
-            else beforeLastPos = transform.position;
-            spawnPos = 2* lastPos - beforeLastPos;
-            spawnRot = links[links.Count - 1].transform.rotation;
-        }
-        GameObject newLink = Instantiate(linkPrefab, spawnPos, spawnRot);
-        newLink.GetComponent<HingeJoint2D>().enabled = false;
-        last.GetComponent<HingeJoint2D>().enabled = true;
-        last.GetComponent<HingeJoint2D>().connectedBody = newLink.GetComponent<Rigidbody2D>();
+        //Vector2 lastPos;
+        //Vector2 spawnPos;
+        //GameObject last;
+        //Quaternion spawnRot;
+        //if (links.Count == 0)
+        //{
+        //    last = this.gameObject;
+        //    if (headBody.velocity.magnitude > 0)
+        //    {
+        //        spawnPos = headBody.velocity;
+        //        spawnPos.Normalize();
+        //    }
+        //    else spawnPos = Vector2.left;
+        //    spawnPos = new Vector2(transform.position.x, transform.position.y) - (spawnPos * spawnDistance);
+        //    spawnRot = transform.rotation;
+        //}
+        //else
+        //{
+        //    last = links[links.Count - 1];
+        //    lastPos = last.transform.position;
+        //    Vector2 beforeLastPos;
+        //    if (links.Count > 1) beforeLastPos = links[links.Count - 2].transform.position;
+        //    else beforeLastPos = transform.position;
+        //    spawnPos = 2* lastPos - beforeLastPos;
+        //    spawnRot = links[links.Count - 1].transform.rotation;
+        //}
+        //GameObject newLink = Instantiate(linkPrefab, spawnPos, spawnRot);
+        //newLink.GetComponent<HingeJoint2D>().enabled = false;
+        //last.GetComponent<HingeJoint2D>().enabled = true;
+        //last.GetComponent<HingeJoint2D>().connectedBody = newLink.GetComponent<Rigidbody2D>();
+        //newLink.transform.parent = playerParent.transform;
+        //links.Add(newLink);
+
+
+        GameObject lastLink = (playerNum == 1) ? lastLink1 : lastLink2;
+        Vector2 spawnPos = midLink.transform.position + (lastLink.transform.position - midLink.transform.position) / 2;
+        GameObject newLink = Instantiate(linkPrefab, spawnPos, lastLink.transform.rotation);
+        lastLink.GetComponent<HingeJoint2D>().connectedBody = newLink.GetComponent<Rigidbody2D>();
+        newLink.GetComponent<HingeJoint2D>().connectedBody = midLink.GetComponent<Rigidbody2D>();
         newLink.transform.parent = playerParent.transform;
-        links.Add(newLink);
+        //newLink.GetComponent<HingeJoint2D>().enabled = false;
+        //last.GetComponent<HingeJoint2D>().enabled = true;
+        //last.GetComponent<HingeJoint2D>().connectedBody = newLink.GetComponent<Rigidbody2D>();
+        //newLink.transform.parent = playerParent.transform;
+        //links.Add(newLink);
+
+
     }
 
     // Update is called once per frame
@@ -82,34 +110,32 @@ public class BodyScript : MonoBehaviour
         {
             addLink();
         }
-        if (headBody.velocity.magnitude > 0.1f)
-        {
-            float angle = Mathf.Atan2(headBody.velocity.y, headBody.velocity.x) * Mathf.Rad2Deg + 180;
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        }
+        //if (headBody.velocity.magnitude > 0.1f)
+        //{
+        //    float angle = Mathf.Atan2(headBody.velocity.y, headBody.velocity.x) * Mathf.Rad2Deg + 180;
+        //    transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        //}
     }
 
     void checkFoodPickup(GameObject link)
     {
-        List<Vector3> vertices = new List<Vector3>();
-        vertices.Add(transform.position);
-        int i = 1;
-        while(links[i - 1] != link)
-        {
-            vertices.Add(links[i - 1].transform.position);
-            i++;
-        }
-        int[] triangles = new int[vertices.Count * 3];
-        for (i = 0; i < vertices.Count - 2; i++)
-        {
-            triangles[i * 3] = i + 2;
-            triangles[i * 3 + 1] = 0;
-            triangles[i * 3 + 2] = i + 1;
-        }
-        Mesh container = new Mesh();
-        container.vertices = vertices.ToArray();
-        container.triangles = triangles;
-
+        //List<Vector2> vertices = new List<Vector2>();
+        //vertices.Add(transform.position);
+        //int i = 1;
+        //while (links[i - 1] != link)
+        //{
+        //    vertices.Add(links[i - 1].transform.position);
+        //    i++;
+        //}
+        //Vector2[] verArr = vertices.ToArray();
+        //foreach (GameObject food in manager.foods)
+        //{
+        //    if (Poly.ContainsPoint(verArr, food.transform.position))
+        //    {
+        //        Debug.Log("ate!"); // TODO delete and respawn
+        //        addLink();
+        //    }
+        //}
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -117,6 +143,24 @@ public class BodyScript : MonoBehaviour
         if (collision.gameObject.tag == "link")
         {
             checkFoodPickup(collision.gameObject);
+        }
+    }
+
+    public static class Poly
+    {
+        public static bool ContainsPoint(Vector2[] polyPoints, Vector2 p)
+        {
+            var j = polyPoints.Length - 1;
+            var inside = false;
+            for (int i = 0; i < polyPoints.Length; j = i++)
+            {
+                var pi = polyPoints[i];
+                var pj = polyPoints[j];
+                if (((pi.y <= p.y && p.y < pj.y) || (pj.y <= p.y && p.y < pi.y)) &&
+                    (p.x < (pj.x - pi.x) * (p.y - pi.y) / (pj.y - pi.y) + pi.x))
+                    inside = !inside;
+            }
+            return inside;
         }
     }
 }
---
+++ Assets/Scripts/JachManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JachManager : MonoBehaviour
+{
+    public List<GameObject> foods;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foods = new List<GameObject>();
+        foods.Add(GameObject.Find("Egg"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
